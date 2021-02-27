6fa33ebf291254536bbd02550dd8878de873a8a0
k4ff <main.kaff@gmail.com>
2021-02-27 16:41:24 +0700

    Initial Commit

---
+++ KaffToolkit/KaffToolkit.h
@@ -0,0 +1,18 @@
+//
+//  KaffToolkit.h
+//  KaffToolkit
+//
+//  Created by Ricki Bin Yamin on 27/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KaffToolkit.
+FOUNDATION_EXPORT double KaffToolkitVersionNumber;
+
+//! Project version string for KaffToolkit.
+FOUNDATION_EXPORT const unsigned char KaffToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KaffToolkit/PublicHeader.h>
+
+
